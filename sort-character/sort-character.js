e3c394ffa6c1264eed6a0bc08e3ed03680819b5a
Sunan Alit <sunanalit@gmail.com>
2025-03-26 14:39:35 +0700

    [ADD] a working sort by appearance letter shorting

--- sort-character/sort-character.js
+++ sort-character/sort-character.js
@@ -8,6 +8,8 @@ const rl = readline.createInterface({
 const sortCharacters = () => {
   let vowels = "";
   let consonants = "";
+  let sortedVowels = "";
+  let sortedConsonants = "";
   let words = "";
 
   rl.question("Input one line of words (S) : ", (input) => {
@@ -19,15 +21,40 @@ const sortCharacters = () => {
     for (let i = 0; i < word.length; i++) {
       if (word[i].match(/[aeiou]/)) {
         vowels += word[i];
-      } else {
+      } else if (word[i].match(/[a-z]/)) {
         consonants += word[i];
+      }
+    }
+    rl.close();
+  };
+
+  const sortByAppearanceVowel = () => {
+    for (let i = 0; i < vowels.length; i++) {
+      if (!sortedVowels.includes(vowels[i])) {
+        for (let j = 0; j < vowels.length; j++) {
+          if (vowels[j] === vowels[i]) {
+            sortedVowels += vowels[j];
+          }
+        }
+      }
+    }
+  };
+
+  const sortByAppearanceConsonant = () => {
+    for (let i = 0; i < consonants.length; i++) {
+      if (!sortedConsonants.includes(consonants[i])) {
+        for (let j = 0; j < consonants.length; j++) {
+          if (consonants[j] === consonants[i]) {
+            sortedConsonants += consonants[j];
+          }
+        }
       }
     }
   };
 
   rl.on("close", () => {
-    let sortedVowels = vowels.split("").join("");
-    let sortedConsonants = consonants.split("").join("");
+    sortByAppearanceVowel();
+    sortByAppearanceConsonant();
 
     console.log("Vowel Characters : ");
     console.log(sortedVowels);
